e95197cc92f30bd94e5bf6221e75c6c1cb60f134
Penugonda <sundara.k.penugonda@accenture.com>
2023-06-18 04:10:42 +0530

    second

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,39 +1,36 @@
 import Proj1 from "../assets/proj1.jpg";
 import Proj2 from "../assets/proj2.jpg";
-import Proj3 from "../assets/proj3.webp";
-import Proj4 from "../assets/proj4.webp";
-import Proj5 from "../assets/proj5.jpg";
-import Proj6 from "../assets/proj6.png";
+import Proj3 from "../assets/proj3.png";
+import Proj4 from "../assets/proj4.jpeg";
+import Proj5 from "../assets/proj5.jpeg";
+
 
 export const ProjectList = [
   {
-    name: "Dijkstra Algorithm Visualizer",
+    name: "Crypto Tracker Project",
     image: Proj1,
-    skills: "JavaScript,HTML,CSS",
+    skills: "Reactjs,JavaScript, Material UI, HTML,CSS",
   },
   {
-    name: "Ecommerce Website",
+    name: "ImageBazaar App Clone",
     image: Proj2,
-    skills: "React,Node.js,MongoDB",
+    skills: "JavaScript, CSS, HTML",
   },
   {
-    name: "Spotify Clone",
+    name: "Tic Tac Toe Game",
     image: Proj3,
-    skills: "React,Node.js,MongoDB,SpotifyAPI",
-  },
-  {
-    name: "Social Media Website",
-    image: Proj4,
-    skills: "React,Node.js,MySQL,GraphQL",
-  },
-  {
-    name: "Dashboard Visualizer",
-    image: Proj5,
     skills: "JavaScript,HTML,CSS",
   },
   {
-    name: "Mobile Game",
-    image: Proj6,
-    skills: "React Native,JavaScript,HTML,CSS",
+    name: "Student Database Management System",
+    image: Proj4,
+    skills: "Java Swing, MySQL",
   },
+  {
+    name: "Compressor Decompressor",
+    image: Proj5,
+    skills: "Java Swing",
+  },
+  
+ 
 ];
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -9,49 +9,51 @@ function Experience() {
   return (
     <div className="experience">
       <VerticalTimeline lineColor="#3e497a">
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2010 - 2014"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon />}
+      <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2021 - present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            My Random High School, Random Place, Random State
+            Front End Developer - Accenture
           </h3>
-          <p> High School Diploma</p>
+          <h4 className="vertical-timeline-element-subtitle">
+            Hyderabad, India
+          </h4>
+          <p>Developed the web portal for Customer Service agents</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2014 - 2018"
+          date="2017 - 2021"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            My Cool University, Vancouver, British Columbia
+            University College of Engineering, JNTUK Narasaraopet
           </h3>
 
           <h4 className="vertical-timeline-element-subtitle">
             Bachelor's Degree
           </h4>
 
-          <p> Computer Science</p>
+          <p> Mechanical Engineering</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2018 - 2020"
-          iconStyle={{ background: "#e9d35b", color: "#fff" }}
-          icon={<WorkIcon />}
+          className="vertical-timeline-element--education"
+          date="2014 - 2015"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Back End Engineer - Google
+            Sri Chaitanya High School
           </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>Developed the backend infrastructure for 3 projects.</p>
+          <p> SSC</p>
         </VerticalTimelineElement>
+       
+        
 
-        <VerticalTimelineElement
+        {/* <VerticalTimelineElement
           className="vertical-timeline-element--work"
           date="2020 - present"
           iconStyle={{ background: "#e9d35b", color: "#fff" }}
@@ -67,7 +69,7 @@ function Experience() {
             Helped the team launch 2 major features by working both in the front
             end and back end.
           </p>
-        </VerticalTimelineElement>
+        </VerticalTimelineElement> */}
       </VerticalTimeline>
     </div>
   );
